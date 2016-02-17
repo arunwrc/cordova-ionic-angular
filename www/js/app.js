33f263de0879a846a1fde2d54ffa93432081f8d6
Arun R <arunr@acube.co>
2016-02-18 01:22:07 +0530

    added extra field and delete implemented

--- www/js/app.js
+++ www/js/app.js
@@ -10,15 +10,34 @@ angular.module('starter', ['ionic', 'firebase'])
     return $firebaseArray(itemsRef);
 }])
 .controller('ListCtrl', function($scope, $ionicListDelegate, Items){
-   $scope.items = Items;
+    $scope.items = Items;
     $scope.addItem = function(){
         var name = prompt("What do you need to buy");
+        //date function
+        var today = new Date();
+        var dd = today.getDate();
+        var mm = today.getMonth()+1; //January is 0!
+        var yyyy = today.getFullYear();
+        var time = today.getHours()+':'+today.getMinutes()+':'+today.getSeconds();
+        var todays_date = dd+'-'+mm+'-'+yyyy+' '+time;
+        //date function
         if (name){
             $scope.items.$add({
-               'name' : name 
+               'name' : name, 
+               'created_at' : todays_date
             });
         }
     };
+    //delete method start
+    $scope.removeItem = function(itemID) {
+        var deleteitem = confirm('Are you absolutely sure you want to delete?');
+        if (deleteitem) {
+            //$http.delete('https://testappcordova.firebaseio.com/items/-KAkdVe4E68DU7ik3ffK');
+            var item = new Firebase('https://testappcordova.firebaseio.com/items/'+itemID);
+            item.remove();  
+        }
+    }
+    //delete method end
     $scope.purchaseItem = function(item){
         $scope.item = item;
         $scope.item['status'] = 'purchased';
