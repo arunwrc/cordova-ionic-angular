aa14ba86656cd057d9a28e41f6054e43a49d5fc0
Arun R <arunr@acube.co>
2016-02-17 17:33:51 +0530

    added backend

--- www/js/app.js
+++ www/js/app.js
@@ -3,14 +3,18 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic'])
+angular.module('starter', ['ionic', 'firebase'])
 
-.controller('ListCtrl', function($scope, $ionicListDelegate){
-   $scope.items = [];
+.factory('Items', ['$firebaseArray', function($firebaseArray){
+    var itemsRef = new Firebase('https://testappcordova.firebaseio.com/items');
+    return $firebaseArray(itemsRef);
+}])
+.controller('ListCtrl', function($scope, $ionicListDelegate, Items){
+   $scope.items = Items;
     $scope.addItem = function(){
         var name = prompt("What do you need to buy");
         if (name){
-            $scope.items.push({
+            $scope.items.$add({
                'name' : name 
             });
         }
